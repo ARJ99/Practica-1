0688d243eccc83464125de4acb0a2eed5644a0ed
AR-PRIME <alejoriios14@gmail.com>
2024-10-11 13:44:47 -0500

    Origen

--- prueba.java
+++ prueba.java
@@ -1,8 +1,5 @@
 class prueba {
 public static void main( String[ ] args) {
-System.out.println(“Hola como estas ???? “);
+System.out.println("Hola, Como estas: " );
 }
 }
-echo 1,2,3,4,5 >>archivodatos1.dat
-echo 6,7,8,9,10 >>archivosdato2.dat
-echo 11,12,13,14 >>archivodatos3.dat
