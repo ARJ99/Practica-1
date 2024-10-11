55733f98463cb75e4a44f85e4a64a093fb03377d
AR-PRIME <alejoriios14@gmail.com>
2024-10-11 12:29:50 -0500

    second commit

--- prueba.java
+++ prueba.java
@@ -1,5 +1,8 @@
 class prueba {
-public static void main( String[] args) {
-System.out.println("Hola como estas ???? ");
+public static void main( String[ ] args) {
+System.out.println(“Hola como estas ???? “);
 }
 }
+echo 1,2,3,4,5 >>archivodatos1.dat
+echo 6,7,8,9,10 >>archivosdato2.dat
+echo 11,12,13,14 >>archivodatos3.dat
